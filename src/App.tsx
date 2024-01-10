3f140b90d382890a73a3198e8c4df853fd260524
Petrus <petrusheya@gmail.com>
2024-01-10 21:46:07 +0200

    hover effects

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import TaskCard from './components/TaskCard'
-import {Task, tasks as initialTasks, statuses} from './utils/data-tasks'
+import {Status, Task, tasks as initialTasks, statuses} from './utils/data-tasks'
 
 function App() {
   const [tasks, setTasks] = useState<Task[]>(initialTasks)
@@ -19,16 +19,33 @@ function App() {
     })
     setTasks(updatedTasks)
   }
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>, status: Status)=>{
+    e.preventDefault()
+    setCurrentlyHovered(null)
+    const id = e.dataTransfer.getData("id") 
+    const task = tasks.find((task)=> task.id === id)
+    if(task){
+      updateTask({...task, status})
+    }
+  }
+
+  const [currentlyHovered, setCurrentlyHovered] = useState <Status | null> (null)
+
+  function handleDragEnter(status: Status){
+    setCurrentlyHovered(status)
+  }
 
   return (
     <div className='flex divide-x'>
       {columns.map((column)=>(
-        <div>
+        <div onDragEnter={()=> handleDragEnter(column.title) } onDrop={(e)=> handleDrop(e, column.title)} onDragOver={((e)=> e.preventDefault())}>
           <div className='flex justify-between items-center text-[30px] p-2 font-bold'>
             <h1 className="capitalize ">{column.title}</h1>
             {column.tasks.reduce((total, task)=> total + (task?.points || 0), 0)}
           </div>
-          {column.tasks.map(((task)=>(<TaskCard task={task} updateTask={updateTask}/>)))}
+          <div className={`h-full ${currentlyHovered === column.title ? 'bg-gray-500': ''}`}>
+            {column.tasks.map(((task)=>(<TaskCard task={task} updateTask={updateTask}/>)))}
+          </div>
       </div>
       ))}
     </div>
